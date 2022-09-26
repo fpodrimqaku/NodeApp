9e4cac877279670e08d07c2e7d50bcdc9dd1f49c
fpodrimqaku <podrimqakuf@gmail.com>
2022-09-27 00:31:47 +0200

    added a logger and error handling

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,77 +1,128 @@
 import { Request, Response, NextFunction } from 'express';
 import { IUserService } from '../services/interfaces/iuser.service';
 import { IUser, GenericResponse } from '../faceModels/index'
+import logger from '../logging/logger'
 export class UsersController {
 
   private readonly _service: IUserService;
 
   constructor(service: IUserService) {
     this._service = service;
+
   }
 
   public async getAllUsers(request: Request, response: Response, next: NextFunction): Promise<any> {
-    let res = await this._service
-      .getUsers();
+    try {
+      let res = await this._service
+        .getUsers();
 
-    return response.status(200).send(res)
+      return response.status(200).send(res)
+    }
+    catch (err) {
+
+      console.log(err);
+      logger.error("Internal error occurred!", err);
+      return response.status(500).send(({
+        successful: false,
+        errors: ['Internal error ocurred!']
+
+      } as any) as GenericResponse<IUser>);
+
+    }
 
   }
 
   public async deleteUser(request: Request, response: Response, next: NextFunction): Promise<any> {
 
-    if (!request.params.id)
-      return response.status(500).send(
-        ({
-          successful: false,
-          errors: ['Mising param Id!']
+    try {
+      if (!request.params.id)
+        return response.status(500).send(
+          ({
+            successful: false,
+            errors: ['Mising param Id!']
 
-        } as any) as GenericResponse<IUser>)
+          } as any) as GenericResponse<IUser>)
 
-    let res = await this._service
-      .deleteUser(new Number(request.params.id));
+      let res = await this._service
+        .deleteUser(new Number(request.params.id));
 
-    if(res.successful)  
-    return response.status(200).send(res)
-    else 
-    return response.status(400).send(res)
+      if (res.successful)
+        return response.status(200).send(res)
+      else
+        return response.status(400).send(res)
+    }
+    catch (err) {
 
+      console.log(err);
+      logger.error("Internal error occurred!", err);
+      return response.status(500).send(({
+        successful: false,
+        errors: ['Internal error ocurred!']
+
+      } as any) as GenericResponse<IUser>);
+
+    }
   };
   public async addUser(request: Request, response: Response, next: NextFunction): Promise<any> {
+    try {
+      if (!request.body.firstName || !request.body.lastName)
+        return response.status(500).send(
+          ({
+            successful: false,
+            errors: ['Specify lastName and firstName in json body!']
 
-    if (!request.body.firstName || !request.body.lastName)
-    return response.status(500).send(
-      ({
+          } as any) as GenericResponse<IUser>)
+
+
+      let res = await this._service
+        .addUser(request.body);
+
+      if (res.successful)
+        return response.status(200).send(res)
+      else
+        return response.status(400).send(res)
+
+    } catch (err) {
+
+      console.log(err);
+      logger.error("Internal error occurred!", err);
+      return response.status(500).send(({
         successful: false,
-        errors: ['Specify lastName and firstName in json body!']
+        errors: ['Internal error ocurred!']
 
-      } as any) as GenericResponse<IUser>)
-
-
-    let res = await this._service
-      .addUser(request.body);
-
-      if(res.successful)
-    return response.status(200).send(res)
-    else 
-     return response.status(400).send(res)
+      } as any) as GenericResponse<IUser>);
+    }
   };
   public async updateUser(request: Request, response: Response, next: NextFunction): Promise<any> {
 
-    if (!request.body.id)
-    return response.status(500).send(
-      ({
+    try {
+
+      if (!request.body.id)
+        return response.status(500).send(
+          ({
+            successful: false,
+            errors: ['Missing id of the user to update!']
+
+          } as any) as GenericResponse<IUser>)
+
+
+      let res = await this._service
+        .updateUser(request.body);
+      if (res.successful)
+        return response.status(200).send(res)
+      else
+        return response.status(400).send(res)
+    }
+    catch (err) {
+
+      console.log(err);
+      logger.error("Internal error occurred!", err);
+      return response.status(500).send(({
         successful: false,
-        errors: ['Missing id of the user to update!']
+        errors: ['Internal error ocurred!']
 
-      } as any) as GenericResponse<IUser>)
-
-
-    let res = await this._service
-      .updateUser(request.body);
-      if(res.successful)
-    return response.status(200).send(res)
-    else
-    return response.status(400).send(res)
+      } as any) as GenericResponse<IUser>);
+    }
   };
 
 
